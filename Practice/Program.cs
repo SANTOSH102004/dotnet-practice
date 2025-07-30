d1893eeb220b46c18ff4d48e288659a0909e8048
santosh <kumbharsantosh348@gmail.com>
2025-07-30 10:01:55 -0700

    new commit

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// int age = 21;
+// float rating = 4.5f;
+// double price = 19.99;
+// char grade = 'A';
+// bool isPass = true;
+
+// Console.WriteLine($"Age: {age}");
+// Console.WriteLine($"Rating: {rating}");         
+// Console.WriteLine($"Price: {price}");
+// Console.WriteLine($"Grade: {grade}");   
+// Console.WriteLine($"Is Pass: {isPass}"); 
+
+int mark = 100;
+
+if (mark >= 90)
+{
+    Console.WriteLine("Grade: A");
+}
+else if (mark >= 80)
+{
+    Console.WriteLine("Grade: B");
+}
+else if (mark >= 70)
+{
+    Console.WriteLine("Grade: C");
+}
+else if (mark >= 60)
+{
+    Console.WriteLine("Grade: D");
+}
+else
+{
+    Console.WriteLine("Grade: F");
+}
