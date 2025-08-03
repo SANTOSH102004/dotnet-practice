9080655d561a0d6b36828539e0032a9d66ec674a
santosh <kumbharsantosh348@gmail.com>
2025-08-02 19:59:46 -0700

    new commit

--- HelloApp/obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
+++ HelloApp/obj/Debug/net9.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b2254a8aad4cbfc1fa505673bcf23710f31a44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36f4da333b5a1ab2ca7956baa99ffd376da37e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
